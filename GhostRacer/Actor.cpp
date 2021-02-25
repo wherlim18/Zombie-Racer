1b330dba299d5c8a4356b1daeea2bde71b1b58f5
William <william.herlim39@gmail.com>
2021-02-26 04:36:18 +0700

    Finish 99%
    Need debugging

--- GhostRacer/Actor.cpp
+++ GhostRacer/Actor.cpp
@@ -3,6 +3,7 @@
 #include "GameConstants.h"
 #include <iostream>
 #include <cmath>
+#include <stdlib.h>
 
 const int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH/2;
 const int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH/2;
@@ -86,6 +87,16 @@ bool Actor::isCollisionAvoidanceWorthy() const
 {
     return false;
 }
+int Actor::doSomethingWhenHit()
+{
+    return -1;
+}
+
+//Actor Spray
+bool Actor::beSprayedIfAppropriate()
+{
+    return false;
+}
 
 
 // Agent Implementations
@@ -110,6 +121,11 @@ void Agent::increaseHP(int hp)
     m_health += hp;
 }
 
+bool Agent::isCollisionAvoidanceWorthy() const
+{
+    return true;
+}
+
 // Ghost Racer Implementations
 GhostRacer::GhostRacer(StudentWorld* world, double x, double y): Agent(world, IID_GHOST_RACER, x, y, 90, 4.0, 0, 0, 100), m_spray(10)
 {
@@ -132,12 +148,17 @@ void GhostRacer::doSomething()
     if(!isAlive())
         return;
     
+    if(getHP() <= 0)
+        killed();
+    
     int ch;
     
     if(getX() <= LEFT_EDGE)
     {
         if(getDirection() > 90)
         {
+            reduceHP(10);
+            getWorld()->playSound(SOUND_VEHICLE_CRASH);
             setDirection(82);
         }
     }
@@ -146,6 +167,8 @@ void GhostRacer::doSomething()
     {
         if(getDirection() < 90)
         {
+            reduceHP(10);
+            getWorld()->playSound(SOUND_VEHICLE_CRASH);
             setDirection(98);
         }
     }
@@ -187,7 +210,45 @@ void GhostRacer::doSomething()
                     changeVerticalSpeed(-1);
                 }
                 break;
-            } 
+            }
+            
+            case KEY_PRESS_SPACE:
+            {
+                if(getSpray() >= 1)
+                {
+                    double cur_dir = getDirection();
+                    double abs_dir = abs(90-cur_dir);
+                    double delta_x;
+                    double delta_y;
+                    double new_x;
+                    double new_y;
+                    
+                    delta_x = sin(abs_dir * (PI/180)) * SPRITE_HEIGHT;
+                    delta_y = cos(abs_dir * (PI/180)) * SPRITE_HEIGHT;
+                    
+                    if(cur_dir > 90)
+                    {
+                        new_x = getX() - delta_x;
+                        new_y = getY() + delta_y;
+                        getWorld()->addNewSpray(new_x, new_y, cur_dir);
+                    }
+                    else if (cur_dir < 90)
+                    {
+                        new_x = getX() + delta_x;
+                        new_y = getY() + delta_y;
+                        getWorld()->addNewSpray(new_x, new_y, cur_dir);
+                    }
+                    else
+                    {
+                        new_y = getY() + SPRITE_HEIGHT;
+                        getWorld()->addNewSpray(getX(), new_y, cur_dir);
+                    }
+                    
+                    getWorld()->playSound(SOUND_PLAYER_SPRAY);
+                    increaseSprays(-1);
+                }
+                break;
+            }
         }
     }
     moveGhostRacer();
@@ -199,9 +260,34 @@ int GhostRacer::getSpray() const
     return m_spray;
 }
 
+void GhostRacer::increaseSprays(int amount)
+{
+    m_spray += amount;
+}
+
+// Ghost Racer Activated Objects Implementations
+ 
+GhostRacerActivatedObject::GhostRacerActivatedObject(StudentWorld* world, int imageID, double x, double y, double size, int dir): Actor(world, imageID, x, y, dir, size, 2, -4, 0)
+{
+    
+}
+
+bool GhostRacerActivatedObject::beSprayedIfAppropriate()
+{
+    if(isSprayable())
+    {
+        if(selfDestructs())
+        {
+            killed();
+        }
+        return true;
+    }
+    return false;
+}
+
 // Border Line Implementations
 
-BorderLine::BorderLine(StudentWorld* world, int imageID, double startX, double startY): Actor(world, imageID, startX, startY, 0, 2.0, 1, -4, 0)
+BorderLine::BorderLine(StudentWorld* world, int imageID, double startX, double startY): Actor(world, imageID, startX, startY, 0, 2.0, 2, -4, 0)
 {
     
 }
@@ -226,17 +312,58 @@ void BorderLine::doSomething()
     
 }
 
+// Spray Implementations
+
+Spray::Spray(StudentWorld* world, double x, double y, int dir): Actor(world, IID_HOLY_WATER_PROJECTILE, x, y, dir, 1.0, 1, 0, 0), m_maximumTravel(160)
+{
+    
+}
+
+int Spray::getMaximumTravel() const
+{
+    return m_maximumTravel;
+}
+
+void Spray::setMaximumTravel(int max)
+{
+    m_maximumTravel = max;
+}
+
+void Spray::doSomething()
+{
+    if(!isAlive())
+        return;
+    
+    if(getWorld()->sprayFirstAppropriateActor(this))
+    {
+        killed();
+        return;
+    }
+    
+    moveForward(SPRITE_HEIGHT);
+    setMaximumTravel(getMaximumTravel() - SPRITE_HEIGHT);
+    
+    
+    if(getX() < 0 || getY() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT)
+    {
+        killed();
+        return;
+    }
+    
+    if(getMaximumTravel() <= 0)
+    {
+        killed();
+        return;
+    }
+    
+    
+}
 
 // Pedestrian Implementations
 
 Pedestrian:: Pedestrian(StudentWorld* world, int imageID, double startX, double startY, double size): Agent(world, imageID, startX, startY, 0, size, -4, 0, 2)
 {
     
-}
-
-bool Pedestrian::isCollisionAvoidanceWorthy() const
-{
-    return true;
 }
 
 void Pedestrian::moveAndPossiblyPickPlan()
@@ -277,6 +404,14 @@ void Pedestrian::setMovementPlanDistance(int distance)
 HumanPedestrian:: HumanPedestrian(StudentWorld* world, double startX, double startY): Pedestrian(world, IID_HUMAN_PED, startX, startY, 2.0)
 {
 
+}
+
+bool HumanPedestrian::beSprayedIfAppropriate()
+{
+    setHorizontalSpeed(getHorizontalSpeed()*-1);
+    setDirection(180-getDirection());
+    getWorld()->playSound(SOUND_PED_HURT);
+    return true;
 }
 
 void HumanPedestrian::doSomething()
@@ -324,6 +459,7 @@ void HumanPedestrian::doSomething()
 int HumanPedestrian::doSomethingWhenHit()
 {
     getWorld()->decLives();
+    getWorld()->resetBonusPoints();
     return GWSTATUS_PLAYER_DIED;
 }
 
@@ -345,6 +481,7 @@ void ZombiePedestrian::doSomething()
         killed();
         return;
     }
+    
     
     if(abs(getWorld()->getPlayer()->getX() - getX()) <= 30 && getY() > getWorld()->getPlayer()->getY())
     {
@@ -422,4 +559,384 @@ void ZombiePedestrian::setUntilGrunt(int ticks)
 void ZombiePedestrian::changeUntilGrunt(int ticks)
 {
     m_untilGrunt += ticks;
+}
+
+bool ZombiePedestrian::beSprayedIfAppropriate()
+{
+    reduceHP(1);
+    
+    if(getHP() <= 0)
+    {
+        killed();
+        
+        getWorld()->playSound(SOUND_PED_DIE);
+        
+        int random = randInt(1, 5);
+        
+        if(random == 1)
+        {
+            if(!getWorld()->overlaps(this, getWorld()->getPlayer()))
+            {
+                getWorld()->addNewHealingGoodie(getX(), getY());
+            }
+        }
+        
+        getWorld()->increaseScore(150);
+    }
+    
+    return true;
+}
+
+// Zombie Cab Implementations
+
+ZombieCab::ZombieCab(StudentWorld* world, double x, double y, int verticalSpeed): Agent(world, IID_ZOMBIE_CAB, x, y, 90, 4.0, verticalSpeed, 0, 3)
+{
+    
+}
+
+bool ZombieCab::isCollisionAvoidanceWorthy() const
+{
+    return true;
+}
+
+bool ZombieCab::hasDamaged() const
+{
+    return m_hasDamaged;
+}
+
+void ZombieCab::isDamaging()
+{
+    m_hasDamaged = true;
+}
+
+void ZombieCab::moveAndPossiblyPickPlan()
+{
+    int vert_speed = getVerticalSpeed() - getWorld()->getPlayer()->getVerticalSpeed();
+    int horiz_speed = getHorizontalSpeed();
+    
+    int new_y = getY() + vert_speed;
+    int new_x = getX() + horiz_speed;
+    
+    this->moveTo(new_x, new_y);
+    
+    if(getX() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT || getY() < 0)
+    {
+        this->killed();
+        return;
+    }
+}
+
+int ZombieCab::getMovementPlanDistance() const
+{
+    return m_movementPlanDistance;
+}
+
+void ZombieCab::setMovementPlanDistance(int distance)
+{
+    m_movementPlanDistance = distance;
+}
+
+void ZombieCab::doSomething()
+{
+    if(!isAlive())
+        return;
+    
+    if(getWorld()->overlaps(this, getWorld()->getPlayer()))
+    {
+        if(!hasDamaged())
+        {
+            getWorld()->playSound(SOUND_VEHICLE_CRASH);
+            getWorld()->getPlayer()->reduceHP(20);
+            
+            if(getX() <= getWorld()->getPlayer()->getX())
+            {
+                setHorizontalSpeed(-5);
+                
+                int random = randInt(0, 19);
+                
+                setDirection(120+random);
+            }
+            else
+            {
+                setHorizontalSpeed(5);
+                
+                int random = randInt(0, 19);
+                
+                setDirection(60+random);
+            }
+            
+            isDamaging();
+        }
+    }
+    
+    moveAndPossiblyPickPlan();
+    
+    if(getVerticalSpeed() > getWorld()->getPlayer()->getVerticalSpeed())
+    {
+        if(getWorld()->zombieCabDetect(this, 1))
+        {
+            setVerticalSpeed(getVerticalSpeed() - 0.5);
+            return;
+        }
+    }
+   
+    if(getVerticalSpeed() <= getWorld()->getPlayer()->getVerticalSpeed())
+    {
+        if(getWorld()->zombieCabDetect(this, 0))
+        {
+            setVerticalSpeed(getVerticalSpeed() + 0.5);
+            return;
+        }
+    }
+    
+    setMovementPlanDistance(getMovementPlanDistance()-1);
+    
+    if(getMovementPlanDistance() > 0)
+        return;
+    
+    int random = randInt(4, 32);
+    
+    setMovementPlanDistance(random);
+    
+    random = randInt(-2, 2);
+    
+    changeVerticalSpeed(random);
+    
+}
+
+
+// Oil Slick Implementations
+
+OilSlick::OilSlick(StudentWorld* world, double x, double y): GhostRacerActivatedObject(world, IID_OIL_SLICK, x, y, randInt(2, 5), 0)
+{
+    
+}
+
+void OilSlick::doSomething()
+{
+    int vert_speed = getVerticalSpeed() - getWorld()->getPlayer()->getVerticalSpeed();
+    int horiz_speed = getHorizontalSpeed();
+    
+    int new_y = getY() + vert_speed;
+    int new_x = getX() + horiz_speed;
+    
+    this->moveTo(new_x, new_y);
+    
+    if(getX() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT || getY() < 0)
+    {
+        this->killed();
+        return;
+    }
+    
+    if(getWorld()->overlaps(this, getWorld()->getPlayer()))
+    {
+        getWorld()->playSound(SOUND_OIL_SLICK);
+        doActivity(getWorld()->getPlayer());
+    }
+}
+
+void OilSlick::doActivity(GhostRacer* gr)
+{
+    int random = randInt(5, 20);
+    int random_multiplier = randInt(0, 1);
+    
+    int cur_dir = gr->getDirection();
+    
+    if(random_multiplier == 0)
+    {
+        gr->setDirection(cur_dir - random);
+    }
+    else
+    {
+        gr->setDirection(cur_dir + random);
+    }
+    
+    int new_dir = gr->getDirection();
+    
+    if(new_dir < 60)
+    {
+        gr->setDirection(60);
+    }
+    else if (new_dir > 120)
+    {
+        gr->setDirection(120);
+    }
+}
+
+int OilSlick::getScoreIncrease() const
+{
+    return 0;
+}
+
+bool OilSlick::selfDestructs() const
+{
+    return false;
+}
+
+bool OilSlick::isSprayable() const
+{
+    return false;
+}
+
+//Healing Goodie Implementations
+
+HealingGoodie::HealingGoodie(StudentWorld* world, double x, double y):GhostRacerActivatedObject(world, IID_HEAL_GOODIE, x, y, 1.0, 0)
+{
+    
+}
+
+void HealingGoodie::doSomething()
+{
+    int vert_speed = getVerticalSpeed() - getWorld()->getPlayer()->getVerticalSpeed();
+    int horiz_speed = getHorizontalSpeed();
+    
+    int new_y = getY() + vert_speed;
+    int new_x = getX() + horiz_speed;
+    
+    this->moveTo(new_x, new_y);
+    
+    if(getX() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT || getY() < 0)
+    {
+        this->killed();
+        return;
+    }
+    
+    if(getWorld()->overlaps(this, getWorld()->getPlayer()))
+    {
+        doActivity(getWorld()->getPlayer());
+    }
+}
+
+void HealingGoodie::doActivity(GhostRacer* gr)
+{
+    gr->increaseHP(10);
+    
+    if(gr->getHP() > 100)
+    {
+        gr->reduceHP(gr->getHP()-100);
+    }
+    
+    killed();
+    getWorld()->playSound(SOUND_GOT_GOODIE);
+    getWorld()->increaseScore(getScoreIncrease());
+    
+}
+
+int HealingGoodie::getScoreIncrease() const
+{
+    return 250;
+}
+
+bool HealingGoodie::selfDestructs() const
+{
+    return true;
+}
+
+bool HealingGoodie::isSprayable() const
+{
+    return true;
+}
+
+//Holy Water Goodie Implementations
+HolyWaterGoodie::HolyWaterGoodie(StudentWorld* world, double x, double y): GhostRacerActivatedObject(world, IID_HOLY_WATER_GOODIE, x, y, 2.0, 90)
+{
+    
+}
+
+void HolyWaterGoodie::doSomething()
+{
+    int vert_speed = getVerticalSpeed() - getWorld()->getPlayer()->getVerticalSpeed();
+    int horiz_speed = getHorizontalSpeed();
+    
+    int new_y = getY() + vert_speed;
+    int new_x = getX() + horiz_speed;
+    
+    this->moveTo(new_x, new_y);
+    
+    if(getX() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT || getY() < 0)
+    {
+        this->killed();
+        return;
+    }
+    
+    if(getWorld()->overlaps(this, getWorld()->getPlayer()))
+    {
+        doActivity(getWorld()->getPlayer());
+    }
+}
+
+void HolyWaterGoodie::doActivity(GhostRacer* gr)
+{
+    gr->increaseSprays(10);
+    killed();
+    getWorld()->playSound(SOUND_GOT_GOODIE);
+    getWorld()->increaseScore(getScoreIncrease());
+}
+
+int HolyWaterGoodie::getScoreIncrease() const
+{
+    return 50;
+}
+
+bool HolyWaterGoodie::selfDestructs() const
+{
+    return true;
+}
+
+bool HolyWaterGoodie::isSprayable() const
+{
+    return true;
+}
+
+// Soul Goodie Implementations
+SoulGoodie::SoulGoodie(StudentWorld* world, double x, double y):GhostRacerActivatedObject(world, IID_SOUL_GOODIE, x, y, 4.0, 0)
+{
+    
+}
+
+void SoulGoodie::doSomething()
+{
+    int vert_speed = getVerticalSpeed() - getWorld()->getPlayer()->getVerticalSpeed();
+    int horiz_speed = getHorizontalSpeed();
+    
+    int new_y = getY() + vert_speed;
+    int new_x = getX() + horiz_speed;
+    
+    this->moveTo(new_x, new_y);
+    
+    if(getX() < 0 || getX() > VIEW_WIDTH || getY() > VIEW_HEIGHT || getY() < 0)
+    {
+        this->killed();
+        return;
+    }
+    
+    if(getWorld()->overlaps(this, getWorld()->getPlayer()))
+    {
+        doActivity(getWorld()->getPlayer());
+    }
+    
+    setDirection(getDirection()-10);
+}
+
+void SoulGoodie::doActivity(GhostRacer* gr)
+{
+    getWorld()->recordSoulSaved();
+    killed();
+    getWorld()->playSound(SOUND_GOT_SOUL);
+    getWorld()->increaseScore(getScoreIncrease());
+}
+
+int SoulGoodie::getScoreIncrease() const
+{
+    return 100;
+}
+
+bool SoulGoodie::selfDestructs() const
+{
+    return false;
+}
+
+bool SoulGoodie::isSprayable() const
+{
+    return false;
 }
--- GhostRacer/StudentWorld.cpp
+++ GhostRacer/StudentWorld.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <array>
 #include <algorithm>
 #include "Actor.h"
 
@@ -85,13 +86,6 @@ int StudentWorld::move()
     // This code is here merely to allow the game to build, run, and terminate after you hit enter.
     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
     
-    //Set Game Stat
-    ostringstream oss;
-    oss << "Score: " << getScore() << setw(7) << "Lvl: " << getLevel() << setw(14) << "Souls2Save: " << 2*getLevel()+5 << setw(9) << "Lives: " << getLives() << setw(10) << "Health: " << getPlayer()->getHP() << setw(10) << "Sprays: " << getPlayer()->getSpray() << setw(9) << "Bonus: " << getScore();
-    string s = oss.str();
-    
-    setGameStatText(s);
-    
     // Tell ghost racer to move
     if (m_player->isAlive())
     {
@@ -119,6 +113,15 @@ int StudentWorld::move()
         if(!getPlayer()->isAlive())
         {
             return GWSTATUS_PLAYER_DIED;
+        }
+        
+        if(getSoulSaved() == 2*getLevel()+5)
+        {
+            this->playSound(SOUND_FINISHED_LEVEL);
+            resetSoulSaved();
+            increaseScore(getBonusPoints());
+            resetBonusPoints();
+            return GWSTATUS_FINISHED_LEVEL;
         }
     }
     
@@ -182,6 +185,59 @@ int StudentWorld::move()
         m_actors.push_front(new ZombiePedestrian(this, rand_x, VIEW_HEIGHT));
     }
     
+    //Oil Slick
+    int ChanceOilSlick = max(150 - (getLevel()*10), 40);
+    
+    random = randInt(0, ChanceOilSlick-1);
+    
+    if(random == 0)
+    {
+        int rand_x = randInt(LEFT_EDGE, RIGHT_EDGE);
+        m_actors.push_front(new OilSlick(this, rand_x, VIEW_HEIGHT));
+    }
+    
+    //Holy Water Refill Goodies
+    
+    int ChanceOfHolyWater = 100 + 10 * getLevel();
+    random = randInt(0, ChanceOfHolyWater-1);
+    
+    if(random == 0)
+    {
+        int rand_x = randInt(LEFT_EDGE, RIGHT_EDGE);
+        m_actors.push_front(new HolyWaterGoodie(this, rand_x, VIEW_HEIGHT));
+    }
+    
+    //Lost Soul Goodies
+    
+    int ChanceOfLostSoul = 100;
+    random = randInt(0, ChanceOfLostSoul-1);
+    
+    if(random == 0)
+    {
+        int rand_x = randInt(LEFT_EDGE, RIGHT_EDGE);
+        m_actors.push_front(new SoulGoodie(this, rand_x, VIEW_HEIGHT));
+    }
+    
+    // Zombie Cab
+    
+    int ChanceVehicle = max(100 - (getLevel()*10), 20);
+    random = randInt(0, ChanceVehicle-1);
+    
+    if(random == 0)
+    {
+        determineZombieCabSpawn();
+    }
+    
+    //Set Game Stats
+    ostringstream oss;
+    oss << "Score: " << getScore() << setw(7) << "Lvl: " << getLevel() << setw(14) << "Souls2Save: " << 2*getLevel()+5 - getSoulSaved() << setw(9) << "Lives: " << getLives() << setw(10) << "Health: " << getPlayer()->getHP() << setw(10) << "Sprays: " << getPlayer()->getSpray() << setw(9) << "Bonus: " << getBonusPoints();
+    string s = oss.str();
+    
+    setGameStatText(s);
+    
+    reduceBonusPoints();
+    
+    //Continue Game
     return GWSTATUS_CONTINUE_GAME;
 }
 
@@ -239,6 +295,21 @@ bool StudentWorld::overlaps(Actor* subject, Actor* object) const
     
 }
 
+void StudentWorld::recordSoulSaved()
+{
+    m_soulSaved += 1;
+}
+
+int StudentWorld::getSoulSaved() const
+{
+    return m_soulSaved;
+}
+
+void StudentWorld::resetSoulSaved()
+{
+    m_soulSaved = 0;
+}
+
 bool StudentWorld::isCollisionWorthyObjects(Actor* p)
 {
     if(p->isCollisionAvoidanceWorthy())
@@ -249,3 +320,323 @@ bool StudentWorld::isCollisionWorthyObjects(Actor* p)
     return false;
 }
 
+bool StudentWorld::sprayFirstAppropriateActor(Actor *p)
+{
+    list<Actor*>::iterator it = m_actors.begin();
+    
+    for(; it != m_actors.end(); it++)
+    {
+        if(overlaps(p, *it))
+        {
+            if((*it)->beSprayedIfAppropriate())
+                return true;
+        }
+    }
+    return false;
+}
+
+void StudentWorld::addNewSpray(double x, double y, int dir)
+{
+    m_actors.push_front(new Spray(this, x, y, dir));
+}
+
+void StudentWorld::addNewHealingGoodie(double x, double y)
+{
+    m_actors.push_front(new HealingGoodie(this, x, y));
+}
+bool StudentWorld::zombieCabDetect(ZombieCab *p, int direction)
+{
+    int current_x = p->getX();
+    int x_min = 0;
+    int x_max = 0;
+    
+    //Determine lane
+    if(current_x >= LEFT_EDGE && current_x < LEFT_EDGE + ROAD_WIDTH/3)
+    {
+        x_min = LEFT_EDGE;
+        x_max = LEFT_EDGE + ROAD_WIDTH/3;
+    }
+    else if(current_x >= LEFT_EDGE + ROAD_WIDTH/3 && current_x < RIGHT_EDGE-ROAD_WIDTH/3)
+    {
+        x_min = LEFT_EDGE+ROAD_WIDTH/3;
+        x_max = RIGHT_EDGE-ROAD_WIDTH/3;
+    }
+    else
+    {
+        x_min = RIGHT_EDGE-ROAD_WIDTH/3;
+        x_max = RIGHT_EDGE;
+    }
+    
+    int current_y = p->getY();
+    
+    int closest_y = 99999;
+    
+    Actor* target = nullptr;
+    
+    // Check for collision-avoidance worthy actor BEHIND zombie cab
+    if(direction == 0)
+    {
+        list<Actor*>::iterator it = m_actors.begin();
+        
+        for(; it != m_actors.end(); it++)
+        {
+            int object_x = (*it)->getX();
+            
+            if((*it)->isCollisionAvoidanceWorthy() && object_x >= x_min && object_x < x_max && (*it) != p)
+            {
+                int difference = (*it)->getY() - current_y;
+                if(abs(difference) < closest_y && difference < 0)
+                {
+                    closest_y = difference;
+                    target = (*it);
+                }
+            }
+        }
+        
+        if(getPlayer()->getX() >= x_min && getPlayer()->getX() < x_max)
+        {
+            int difference = getPlayer()->getY() - current_y;
+            if(abs(difference) < closest_y && difference < 0)
+            {
+                closest_y = difference;
+                target = getPlayer();
+            }
+        }
+    }
+    
+    // Check for collision-avoidance worthy actor in FRONT zombie cab
+    else if (direction == 1)
+    {
+        list<Actor*>::iterator it = m_actors.begin();
+        
+        for(; it != m_actors.end(); it++)
+        {
+            int object_x = (*it)->getX();
+            
+            if((*it)->isCollisionAvoidanceWorthy() && object_x >= x_min && object_x < x_max && (*it) != p)
+            {
+                int difference = (*it)->getY() - current_y;
+                if(abs(difference) < closest_y && difference > 0)
+                {
+                    closest_y = difference;
+                    target = (*it);
+                }
+            }
+        }
+        
+        if(getPlayer()->getX() >= x_min && getPlayer()->getX() < x_max)
+        {
+            int difference = getPlayer()->getY() - current_y;
+            if(abs(difference) < closest_y && difference > 0)
+            {
+                closest_y = difference;
+                target = (*it);
+            }
+        }
+    }
+    
+    if(abs(closest_y) < 96 && target != nullptr && target != getPlayer())
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+void StudentWorld::determineZombieCabSpawn()
+{
+    //Set found to false
+    bool foundSpawnPoint = false;
+    
+    //Get Order
+    int cur_lane = randInt(0, 2);
+    int first = cur_lane;
+    
+    while(cur_lane == first)
+    {
+        cur_lane = randInt(0, 2);
+    }
+    
+    int second = cur_lane;
+    
+    while(cur_lane == first || cur_lane == second)
+    {
+        cur_lane = randInt(0, 2);
+    }
+    
+    int third = cur_lane;
+    
+    //Input order into an array
+    int order[3] = {first, second, third};
+    
+    //Initialize Boundaries
+    int leftBoundary;
+    int rightBoundary;
+    
+    //Initialize chosen values
+    int chosenLane;
+    int chosenY;
+    int chosenX;
+    int chosenSpeed;
+    
+    //Loop 3 times for each lane
+    for(int i = 0; i < 2; i++)
+    {
+        cur_lane = order[i];
+        
+        int closest = 999;
+        
+        if(cur_lane == 0)
+        {
+            leftBoundary = LEFT_EDGE;
+            rightBoundary = LEFT_EDGE + ROAD_WIDTH/3;
+            
+        }
+        else if(cur_lane == 1)
+        {
+            leftBoundary = LEFT_EDGE + ROAD_WIDTH/3;
+            rightBoundary = RIGHT_EDGE - ROAD_WIDTH/3;
+        }
+        
+        else
+        {
+            leftBoundary = RIGHT_EDGE - ROAD_WIDTH/3;
+            rightBoundary = RIGHT_EDGE;
+        }
+        
+        //Check for closest collision avoidance-worhty actors from the BOTTOM
+        Actor* target = nullptr;
+        
+        list<Actor*>::iterator it = m_actors.begin();
+        
+        for(; it != m_actors.end(); it++)
+        {
+            int cur_x = (*it)->getX();
+            int cur_y = (*it)->getY();
+            
+            if(cur_x >= leftBoundary && cur_x < rightBoundary && (*it)->isCollisionAvoidanceWorthy())
+            {
+                if(cur_y < closest)
+                {
+                    closest = cur_y;
+                    target = *it;
+                }
+            }
+        }
+        
+        if(getPlayer()->getX() >= leftBoundary && getPlayer()->getX() < rightBoundary)
+        {
+            {
+                if(getPlayer()->getY() < closest)
+                {
+                    closest = getPlayer()->getY();
+                    target = getPlayer();
+                }
+            }
+        }
+        
+        if(target == nullptr)
+        {
+            chosenLane = cur_lane;
+            chosenY = SPRITE_HEIGHT/2;
+            chosenSpeed = getPlayer()->getVerticalSpeed() + randInt(2, 4);
+            foundSpawnPoint = true;
+            break;
+        }
+        else if (target != nullptr && target->getY() > VIEW_HEIGHT/3)
+        {
+            chosenLane = cur_lane;
+            chosenY = SPRITE_HEIGHT/2;
+            chosenSpeed = getPlayer()->getVerticalSpeed() + randInt(2, 4);
+            foundSpawnPoint = true;
+            break;
+        }
+        
+        
+        //Check for collision avoidance-worthy actors from the TOP
+        target = nullptr;
+        it = m_actors.begin();
+        closest = 999;
+        
+        for(; it != m_actors.end(); it++)
+        {
+            int cur_x = (*it)->getX();
+            int cur_y = (*it)->getY();
+            
+            if(cur_x >= leftBoundary && cur_x < rightBoundary && (*it)->isCollisionAvoidanceWorthy())
+            {
+                if(255-cur_y < closest)
+                {
+                    closest = cur_y;
+                    target = *it;
+                }
+            }
+        }
+        
+        if(getPlayer()->getX() >= leftBoundary && getPlayer()->getX() < rightBoundary)
+        {
+            {
+                if(255-getPlayer()->getY() < closest)
+                {
+                    closest = getPlayer()->getY();
+                    target = getPlayer();
+                }
+            }
+        }
+        
+        if(target == nullptr)
+        {
+            chosenLane = cur_lane;
+            chosenY = VIEW_HEIGHT - SPRITE_HEIGHT/2;
+            chosenSpeed = getPlayer()->getVerticalSpeed() - randInt(2, 4);
+            foundSpawnPoint = true;
+            break;
+        }
+        
+        else if (target != nullptr && target->getY() < (VIEW_HEIGHT * 2/3))
+        {
+            chosenLane = cur_lane;
+            chosenY = VIEW_HEIGHT - SPRITE_HEIGHT/2;
+            chosenSpeed = getPlayer()->getVerticalSpeed() - randInt(2, 4);
+            foundSpawnPoint = true;
+            break;
+        }
+        
+    }
+    
+    //If spawn point cannot be located, skip the tick
+    if(foundSpawnPoint == false)
+        return;
+    else
+    {
+        if(chosenLane == 0)
+        {
+            chosenX = ROAD_CENTER-(ROAD_WIDTH/3);
+        }
+        else if(chosenLane == 1)
+        {
+            chosenX = ROAD_CENTER;
+        }
+        else
+        {
+            chosenLane = ROAD_CENTER + (ROAD_WIDTH/3);
+        }
+        
+        m_actors.push_front(new ZombieCab(this, chosenX, chosenY, chosenSpeed));
+    }
+}
+
+void StudentWorld::reduceBonusPoints()
+{
+    m_bonusPoints -= 1;
+}
+
+int StudentWorld::getBonusPoints() const
+{
+    return m_bonusPoints;
+}
+
+void StudentWorld::resetBonusPoints()
+{
+    m_bonusPoints = 5000;
+}
